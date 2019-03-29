2bb11f74e4844ce5a6e6a6a2ac33bbbc7ddcf969
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2019-03-29 15:45:25 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""PyODHeaN solver server
+
+Optimization of District Heating Networks
+
+This package provides the solver server.
+"""
+
+from setuptools import setup, find_packages
+
+# Get the long description from the README file
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyodhean-server',
+    version='0.0',
+    description='Optimization of District Heating Networks',
+    long_description=long_description,
+    # url='https://github.com/Nobatek/pyodhean-server',
+    author='Nobatek',
+    author_email='jlafrechoux@nobatek.com',
+    # license='To be defined',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+        # 'License :: ,
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords=[
+        'District',
+        'Heating',
+        'Network',
+        'Optimization',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'pyodhean',
+    ],
+)
