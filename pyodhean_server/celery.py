5a33908ab5d7a9b483deb434a8d69556cd474825
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2019-06-26 16:08:39 +0200

    Add CELERY_BROKER_URL and CELERY_RESULT_BACKEND config env vars

--- pyodhean_server/celery.py
+++ pyodhean_server/celery.py
@@ -2,6 +2,8 @@
 
 Runs PyODHeaN model solver task asynchronously
 """
+import os
+
 from celery import Celery
 from pyodhean_server.settings import DefaultCeleryConfig
 
@@ -11,3 +13,8 @@ app = Celery(
     include=['pyodhean_server.task'],
 )
 app.config_from_object(DefaultCeleryConfig)
+
+if 'CELERY_BROKER_URL' in os.environ:
+    app.conf.update(broker_url=os.environ['CELERY_BROKER_URL'])
+if 'CELERY_RESULT_BACKEND' in os.environ:
+    app.conf.update(result_backend=os.environ['CELERY_RESULT_BACKEND'])
