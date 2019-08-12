cdb8f11f03c796ab172518d4b3cfb16d124abbf7
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2019-08-12 20:03:18 +0200

    Add test_result_query_multiple_times

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,4 +1,5 @@
 """Resources tests"""
+import time
 import json
 from unittest import mock
 
@@ -7,6 +8,8 @@ import pytest
 from kombu.exceptions import OperationalError
 
 from pyodhean_server.solver.task import solve, CELERY_STATUSES_MAPPING
+
+from .utils import redis_running
 
 
 DUMMY_TASK_ID = '00000000-0000-0000-0000-000000000000'
@@ -48,3 +51,37 @@ class TestResources:
         assert '/tasks/{}/status'.format(DUMMY_TASK_ID) in response.json[
             'message']
 
+    @pytest.mark.skipif(not redis_running(), reason="This test requires Redis")
+    @pytest.mark.usefixtures('init_app')
+    def test_result_query_multiple_times(self, json_input):
+        """Test result can be queried several times
+
+        When using RabbitMQ as result backend (RPC backend), results are meant
+        to be consumed only once: https://github.com/celery/celery/issues/5414.
+        In practice, the result may be lost if queried several times.
+        A reproducible case is when another task ID is queried. Then querying
+        for original task ID will fail on a second attempt.
+        """
+        # Post task
+        response = self.client.post(
+            '/solver/tasks/', data=json.dumps(json_input))
+        task_id = response.json['task_id']
+        # Wait until task is completed
+        while self.client.get(
+                '/solver/tasks/{}/status'.format(task_id)
+        ).json['status'] in ('waiting', 'ongoing'):
+            time.sleep(0.1)
+        # Query result for another task ID
+        response = self.client.get(
+            '/solver/tasks/{}/result'.format(DUMMY_TASK_ID))
+        assert response.status_code == 404
+        # Query task result
+        response = self.client.get('/solver/tasks/{}/result'.format(task_id))
+        assert response.status_code == 200
+        result = response.json
+        assert result['status'] == 'ok'
+        assert 'solution' in result
+        # Query task result again
+        response = self.client.get('/solver/tasks/{}/result'.format(task_id))
+        assert response.status_code == 200
+        assert response.json == result
