4b0a5106f1510423988c0897fefa36492d435016
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2019-08-12 20:08:19 +0200

    Mock call to Redis in test_status_unknown_task

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -21,16 +21,16 @@ class TestResources:
     def test_solve_redis_connection_error(self, json_input):
         with mock.patch.object(solve, 'delay', side_effect=OperationalError):
             response = self.client.post(
-                '/solver/tasks/',
-                data=json.dumps(json_input)
-            )
+                '/solver/tasks/', data=json.dumps(json_input))
         assert response.status_code == 503
 
     @pytest.mark.usefixtures('init_app')
     def test_status_unknown_task(self):
-        response = self.client.get(
-            '/solver/tasks/{}/status'.format(DUMMY_TASK_ID)
-        )
+        with mock.patch.object(
+                solve.backend, 'get_state', return_value='PENDING'
+        ):
+            response = self.client.get(
+                '/solver/tasks/{}/status'.format(DUMMY_TASK_ID))
         assert response.status_code == 404
         assert 'Unknown task ID: {}.'.format(DUMMY_TASK_ID) in response.json[
             'message']
@@ -45,8 +45,7 @@ class TestResources:
                 solve.backend, 'get_state', return_value=status
         ):
             response = self.client.get(
-                '/solver/tasks/{}/result'.format(DUMMY_TASK_ID)
-            )
+                '/solver/tasks/{}/result'.format(DUMMY_TASK_ID))
         assert response.status_code == 404
         assert '/tasks/{}/status'.format(DUMMY_TASK_ID) in response.json[
             'message']
