a2418029faf78e12f8810d3e7d08dad5dda390dd
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2019-08-23 16:32:57 +0200

    Add tests.utils.celery_running

--- tests/utils.py
+++ tests/utils.py
@@ -4,6 +4,12 @@ import json
 from werkzeug.utils import cached_property
 from flask import Response
 import redis
+import celery
+import celery.bin.base
+import celery.bin.control
+import celery.platforms
+
+from pyodhean_server.celery import app as celery_app
 
 
 class JSONResponse(Response):
@@ -26,3 +32,18 @@ def redis_running():
     except redis.exceptions.ConnectionError:
         return False
     return True
+
+
+def celery_running():
+    """Test Celery server is available
+
+    Inspired by https://stackoverflow.com/a/33545849
+    """
+    status = celery.bin.control.status(celery_app)
+    try:
+        status.run()
+        return True
+    except celery.bin.base.Error as exc:
+        if exc.status == celery.platforms.EX_UNAVAILABLE:
+            return False
+        raise
