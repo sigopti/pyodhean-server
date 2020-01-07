188bef918162727d1781f49e3c13b2351d806e76
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2020-01-07 11:11:04 +0100

    Use authentication in examples

--- examples/remote_api.py
+++ examples/remote_api.py
@@ -4,6 +4,7 @@
 import time
 import json
 import argparse
+import base64
 from pprint import pprint
 
 import requests
@@ -11,8 +12,22 @@ import requests
 
 parser = argparse.ArgumentParser(description='Test PyODHeaN API.')
 parser.add_argument('--url', dest='host', required=True, help='API host URL')
+parser.add_argument('--user', dest='user', help='Username')
+parser.add_argument('--password', dest='password', help='Password')
 
 args = parser.parse_args()
+
+headers = {}
+
+if args.user:
+    if args.password is None:
+        parser.error('"password" is required when "user" is specified')
+    creds = base64.b64encode(
+        '{user}:{password}'.format(user=args.user, password=args.password)
+        .encode()
+    ).decode()
+    headers['Authorization'] = 'Basic ' + creds
+
 
 json_input = {
     'nodes': {
@@ -69,17 +84,23 @@ json_input = {
 
 # Send task to solver
 response = requests.post(
-    args.host + '/solver/tasks/', data=json.dumps(json_input))
+    args.host + '/solver/tasks/', data=json.dumps(json_input),
+    headers=headers,
+)
 task_id = response.json()['task_id']
 
 # Check status until it's over
 while requests.get(
-        args.host + '/solver/tasks/{}/status'.format(task_id)
+        args.host + '/solver/tasks/{}/status'.format(task_id),
+        headers=headers,
 ).json()['status'] in ('waiting', 'ongoing'):
     time.sleep(1)
 
 # Result now available
-response = requests.get(args.host + '/solver/tasks/{}/result'.format(task_id))
+response = requests.get(
+    args.host + '/solver/tasks/{}/result'.format(task_id),
+    headers=headers,
+)
 result = response.json()
 
 # Display result
--- examples/remote_api_cli.py
+++ examples/remote_api_cli.py
@@ -5,6 +5,7 @@ import sys
 import time
 import json
 import argparse
+import base64
 from pprint import pprint
 
 import requests
@@ -16,6 +17,8 @@ parser.add_argument(
 parser.add_argument(
     '-i', dest='input_file', required=True, help='Input JSON file'
 )
+parser.add_argument('--user', dest='user', help='Username')
+parser.add_argument('--password', dest='password', help='Password')
 
 args = parser.parse_args()
 
@@ -26,20 +29,37 @@ except IOError as e:
     print('Input file error: {}'.format(e))
     sys.exit()
 
+headers = {}
+
+if args.user:
+    if args.password is None:
+        parser.error('"password" is required when "user" is specified')
+    creds = base64.b64encode(
+        '{user}:{password}'.format(user=args.user, password=args.password)
+        .encode()
+    ).decode()
+    headers['Authorization'] = 'Basic ' + creds
+
 
 # Send task to solver
 response = requests.post(
-    args.host + '/solver/tasks/', data=json.dumps(json_input))
+    args.host + '/solver/tasks/', data=json.dumps(json_input),
+    headers=headers,
+)
 task_id = response.json()['task_id']
 
 # Check status until it's over
 while requests.get(
-        args.host + '/solver/tasks/{}/status'.format(task_id)
+        args.host + '/solver/tasks/{}/status'.format(task_id),
+        headers=headers,
 ).json()['status'] in ('waiting', 'ongoing'):
     time.sleep(1)
 
 # Result now available
-response = requests.get(args.host + '/solver/tasks/{}/result'.format(task_id))
+response = requests.get(
+    args.host + '/solver/tasks/{}/result'.format(task_id),
+    headers=headers,
+)
 result = response.json()
 
 # Display result
