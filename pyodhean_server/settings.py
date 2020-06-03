8cf7845b6070706a05de2b2483fddf6592b879d4
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2020-06-03 16:27:24 +0200

    Rework DefaultConfig: use OPENAPI_REDOC_URL, add comments

--- pyodhean_server/settings.py
+++ pyodhean_server/settings.py
@@ -3,17 +3,33 @@
 
 class DefaultConfig:
     """Default configuration"""
+
+    # pyodhean-server API version
     API_VERSION = 0.1
-    OPENAPI_VERSION = '3.0.2'
-    OPENAPI_URL_PREFIX = '/'
-    OPENAPI_REDOC_PATH = '/'
-    OPENAPI_REDOC_VERSION = 'next'
+
+    # Authentication
     AUTH_ENABLED = False
+
+    # Logging
     LOGGER_ENABLED = False
     LOGGER_LEVEL = 'WARNING'
     LOGGER_FORMAT = (
         '%(asctime)s | %(levelname)-8s | '
         '%(remote_addr)-15s | %(method)-7s | %(url)s | %(message)s'
+    )
+
+    # OpenAPI settings
+    # These settings are used by the automatic swagger API documentation.
+    # They may be overidden to disable it or configure it differently.
+    # In production, it is a good idea to pin the ReDoc version number rather
+    # than use "next".
+    # See flask-smorest documentation:
+    # https://flask-smorest.readthedocs.io/en/latest/openapi.html
+    OPENAPI_VERSION = '3.0.2'
+    OPENAPI_URL_PREFIX = '/'
+    OPENAPI_REDOC_PATH = '/'
+    OPENAPI_REDOC_URL = (
+        "https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"
     )
 
 
