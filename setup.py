c0de48f9941e66451b520a0b1435cb8295f55a6f
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2020-07-10 11:45:27 +0200

    setup.py: require pyodhean>=1.0.0b1

--- setup.py
+++ setup.py
@@ -9,46 +9,46 @@ This package provides the solver server.
 from setuptools import setup, find_packages
 
 # Get the long description from the README file
-with open('README.rst', encoding='utf-8') as f:
+with open("README.rst", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='pyodhean-server',
-    version='0.0',
-    description='Optimization of District Heating Networks',
+    name="pyodhean-server",
+    version="0.0",
+    description="Optimization of District Heating Networks",
     long_description=long_description,
-    url='https://github.com/sigopti/pyodhean-server',
-    author='Nobatek/INEF4',
-    author_email='jlafrechoux@nobatek.com',
+    url="https://github.com/sigopti/pyodhean-server",
+    author="Nobatek/INEF4",
+    author_email="jlafrechoux@nobatek.com",
     keywords=[
-        'District',
-        'Heating',
-        'Network',
-        'Optimization',
+        "District",
+        "Heating",
+        "Network",
+        "Optimization",
     ],
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering",
         (
-            'License :: OSI Approved :: '
-            'GNU Affero General Public License v3 or later (AGPLv3+)'
+            "License :: OSI Approved :: "
+            "GNU Affero General Public License v3 or later (AGPLv3+)"
         ),
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     python_requires=">=3.6",
     install_requires=[
-        'pyodhean',
-        'celery[redis]>=4.2.0,<5.0',
-        'werkzeug>=0.15',
-        'flask>=1.0.0',
-        'python-dotenv>=0.9.0',
-        'flask-smorest>=0.18.0,<0.21',
-        'marshmallow>=3.0.0',
-        'flask_httpauth>=3.0.0'
+        "pyodhean>=1.0.0b1",
+        "celery[redis]>=4.2.0,<5.0",
+        "werkzeug>=0.15",
+        "flask>=1.0.0",
+        "python-dotenv>=0.9.0",
+        "flask-smorest>=0.18.0,<0.21",
+        "marshmallow>=3.0.0",
+        "flask_httpauth>=3.0.0"
     ],
     packages=find_packages(),
 )
